c76e71686a4ba7e870a3319280dbdf2710da4295
Stephen Adeniji <praiseadeniji2017@gmail.com>
2024-02-01 11:10:11 +0100

    new assets

---
+++ assets/svg/OnGoingTicketIcon.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+const OnGoingTicketIcon = () => {
+  return (
+    <svg
+      width="21"
+      height="20"
+      viewBox="0 0 21 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M2.17334 7.08268C2.17334 4.16602 3.84435 2.91602 6.35086 2.91602H14.7059C17.2124 2.91602 18.8834 4.16602 18.8834 7.08268V12.916C18.8834 15.8327 17.2124 17.0827 14.7059 17.0827H6.35086"
+        stroke="#84818A"
+        stroke-width="1.5"
+        stroke-miterlimit="10"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M14.7061 7.5L12.091 9.58333C11.2304 10.2667 9.81841 10.2667 8.95784 9.58333L6.35107 7.5"
+        stroke="#84818A"
+        stroke-width="1.5"
+        stroke-miterlimit="10"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M2.17334 13.75H7.18636"
+        stroke="#84818A"
+        stroke-width="1.5"
+        stroke-miterlimit="10"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M2.17334 10.416H4.67985"
+        stroke="#84818A"
+        stroke-width="1.5"
+        stroke-miterlimit="10"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default OnGoingTicketIcon;
