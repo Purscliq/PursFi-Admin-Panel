49eec6f6356a87f9b7c7047379339e1920204433
roscoericch <sikirurazak1@gmail.com>
2024-02-13 09:46:34 +0100

    update

--- store/store.ts
+++ store/store.ts
@@ -1,22 +1,15 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import { ApiSlice } from "@/services/Api";
-import { CountrySlice } from "@/services/country";
-import userSlice from "./userSlice";
 
 const rootReducer = combineReducers({
   [ApiSlice.reducerPath]: ApiSlice.reducer,
-  [CountrySlice.reducerPath]: CountrySlice.reducer,
-  user: userSlice.reducer,
 });
 
 export const createStore = () =>
   configureStore({
     reducer: rootReducer,
     middleware: (getDefaultMiddleware) =>
-      getDefaultMiddleware().concat([
-        ApiSlice.middleware,
-        CountrySlice.middleware,
-      ]),
+      getDefaultMiddleware().concat([ApiSlice.middleware]),
   });
 
 export type StoreType = ReturnType<typeof createStore>;
